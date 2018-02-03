579444a3d3b15de26a31abee25839e2c760acc3c
Jefferson Miranda Carvalho <miranda.w.sam@outlook.com>
2018-02-03 15:13:23 -0300

    MIDDLEWARE_CLASSES atualizados

--- DjangoTeatroApp/settings.py
+++ DjangoTeatroApp/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE_CLASSES = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
