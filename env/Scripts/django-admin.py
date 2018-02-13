eb55f21091af5ecd853d31b8ad6ec78351d33a70
Jefferson Miranda Carvalho <miranda.w.sam@outlook.com>
2018-02-13 16:58:34 -0300

    .

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\miran\documents\visual studio 2017\projects\djangoteatroapp\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
