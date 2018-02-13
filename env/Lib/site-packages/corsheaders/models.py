134a0afaf48e3e04362ae3a9405acc12e4db7c3b
Jefferson Miranda Carvalho <miranda.w.sam@outlook.com>
2018-02-13 17:08:10 -0300

    Revert "."
    This reverts commit eb55f21091af5ecd853d31b8ad6ec78351d33a70.

--- env/Lib/site-packages/corsheaders/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class AbstractCorsModel(models.Model):
-    class Meta:
-        abstract = True
-        db_table = 'corsheaders_corsmodel'
-
-    cors = models.CharField(max_length=255)
--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\miran\documents\visual studio 2017\projects\djangoteatroapp\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
